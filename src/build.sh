55f61c33cb1bcc01bd0f64fe70932c661057104a
youhide <youri@youhide.com.br>
2025-02-10 12:39:06 -0300

    Remove build script for dotfiles manager

--- src/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Building dotfiles manager..."
-echo "Checking dependencies..."
-go mod tidy
-echo "Compiling..."
-go build -o ../hideDot
-chmod +x hideDot
-echo "Build complete! You can now run ./hideDot"
